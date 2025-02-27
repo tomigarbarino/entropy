3449302435461a34ec120e54eb0deebe7c09a17d
Nico Blasutig <nicolasblasutigdev@gmail.com>
2025-02-27 08:36:52 -0300

    homegrid items

--- frontend/src/app/components/HomeMedia/HomeMedia.jsx
+++ frontend/src/app/components/HomeMedia/HomeMedia.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import "./homeMedia.scss"
 
 const HomeMedia = ({mediaSrc, mediaAlt, mediaNumber, isVideo = false}) => {
   return (
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -14,14 +14,18 @@ export default function Home() {
     <div className="globalContainer">
       <Navbar />
       <div className="home">
-        <HomeMedia mediaSrc={"/videos/uber_home.gif"} />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
-        <HomeMedia mediaSrc={"/images/budweiser_home.png"}  />
+        <div className="row row1">
+          <HomeMedia mediaSrc={"/videos/uber_home.gif"} mediaNumber={1} />
+          <HomeMedia mediaSrc={"/images/budweiser_home.png"} mediaNumber={2}  />
+          <HomeMedia mediaSrc={"/images/stella_home.png"} mediaNumber={3} />
+          <HomeMedia mediaSrc={"/images/santaolalla_home.png"} mediaNumber={4} />
+        </div>
+        <div className="row row2">
+          <HomeMedia mediaSrc={"/images/keller_home.jpg"} mediaNumber={5} />
+          <HomeMedia mediaSrc={"/images/tmr_home.png"} mediaNumber={6} />
+          <HomeMedia mediaSrc={"/images/bayer_home.png"} mediaNumber={7} />
+          <HomeMedia mediaSrc={"/images/panda_home.png"} mediaNumber={8}  />
+        </div>
       </div>
     </div>
     </>
