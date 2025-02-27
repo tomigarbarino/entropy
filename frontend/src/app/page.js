d72bfd20485f32fa5675e0c4af79775f60483c1e
Nico Blasutig <nicolasblasutigdev@gmail.com>
2025-02-27 07:29:20 -0300

    navbar improved

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,16 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.scss";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+
 
 export default function Home() {
+  const [a,b] = useState("sds")
   return (
-    <h1>test</h1>
+    <>
+      <Navbar />
+    </>
   );
 }
