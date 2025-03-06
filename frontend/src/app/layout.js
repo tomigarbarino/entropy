0954dfb8f0518e3ad3c7b36810b409925d7eaebc
tomas garbarino <tomydevtl@gmail.com>
2025-03-06 19:52:28 -0300

    tg/feature/meta

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -26,6 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/entropy_fav.gif" />
       </head>
       <body>
