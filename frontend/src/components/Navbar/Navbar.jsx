8dd7e3f9fea8df401bd3d01670ea1fe48b279f21
tomas garbarino <tomydevtl@gmail.com>
2025-03-07 12:38:09 -0300

    tg/feature-mailito-instyagram

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,14 +11,18 @@ const Navbar = ({showNav}) => {
             <p>CONTACT</p>
             <p>+54 9 2234 540881</p>
             <p>+52 55484 47485 </p>
+            <a href="mailto:contact@entropypro.nyc">
             <p>contact@entropypro.nyc</p>
+            </a>
         </div>
         <div className="logo">
           <img src="/images/ENTROPY_LOGO-12.png" alt="Entropy Logo" />
         </div>
         <div className="social--desktop">
             <p>1NSTAGRAM</p>
-            <p>@entropy.ai</p>
+            <a href="https://www.instagram.com/_entropy.ai/" target="_blank" rel="noopener noreferrer">
+              <p>@_entropy.ai</p>
+            </a>
         </div>
 
         <div className="navbar__bottom">
@@ -26,11 +30,15 @@ const Navbar = ({showNav}) => {
           <DynamicText text="CONTACT" />
           <DynamicText text="+54 9 2234 540881" />
           <DynamicText text="+52 55484 47485" />
+          <a href="mailto:contact@entropypro.nyc">
           <DynamicText text="contact@entropypro.nyc" />
+          </a>
         </div>
         <div className="social">
         <DynamicText text="1NSTAGRAM" />
-        <DynamicText text="@entropy.ai" />
+        <a href="https://www.instagram.com/_entropy.ai/" target="_blank" rel="noopener noreferrer">
+              <DynamicText text="@_entropy.ai" />
+            </a>
         </div>
       </div>
     </nav>
